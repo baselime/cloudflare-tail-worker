a35340feda84162ec42734412696e09b13425c7f
Ankcorn <thomasankcorn@gmail.com>
2023-11-16 11:47:35 +0000

    no catch

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ export interface Env {
 
 export default {
 	async tail(event: TraceItem[], env: Env): Promise<void> {
-		try {
 			const res = await fetch(env.BASELIME_API_URL || "https://cloudflare.baselime.io/v1", {
 				method: "POST",
 				headers: {
@@ -18,14 +17,6 @@ export default {
 				},
 				body: JSON.stringify(event),
 			})
-
-			console.log(res.status);
-			const data = await res.text();
-			console.log(data);
-			console.log('success')
-		} catch(e) {
-			console.error(e);
-		}
 	}
 		
 };
